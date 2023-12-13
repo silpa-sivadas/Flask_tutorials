34afdd8329fd19f41ad008b71b6d9b527ad6a8a4
silpa-sivadas <109214730+silpa-sivadas@users.noreply.github.com>
2023-12-13 12:49:22 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
 
